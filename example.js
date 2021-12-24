66041d5f50ad83cd7bbb4771152f8c4fbc57dd35
Stephen Furlani <stephen.furlani@gmail.com>
2021-12-23 20:07:48 -0500

    Added stuff to the file

---
+++ example.js
@@ -0,0 +1,16 @@
+
+const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
+// A function to Add two numbers together
+function add(x, y) {
+    return x + y
+}
+
+
+const xs = [1, 2, 3, 4, 5, 6, 7]
+const ys = [1, 2, 3, 4, 5, 6, 7]
+var zs = []
+for (let i = 0; i < xs.count; i++) {
+    zs.push(add(xs[i], ys[i]))
+}
+console.log(zs)
